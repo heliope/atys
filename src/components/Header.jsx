3c363f8a6a0b198a6f834d72c91882e3af105d94
Helio <heliopereira-7@hotmail.com>
2023-02-09 22:13:22 +0000

    Implementação e criação do Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faBars, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+export default function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const menuToggle = () => {
+    setMenuOpen(!menuOpen)
+  }
+console.log(menuOpen);
+
+  return (
+    <div className="content-header">
+      <div className="header">
+        <div className="site-logo">
+          <img
+            src="/images/logotipo.png"
+            alt="Logótipo da Atys"
+            title="Atys - Make IT Simple"
+          />
+        </div>
+        <div className="auxiliar"></div>
+        <nav className="nav-header" role="navigation">
+          <div className="link-hamburguer" onClick={menuToggle}>
+              <a className={`hamburguer-${menuOpen ? "list" : "closelist"}`} href="#/"><FontAwesomeIcon icon={faCircleXmark}/></a>
+              <a className={`hamburguer-${menuOpen ? "closelist" : ""}`} href="#/"><FontAwesomeIcon icon={faBars}/></a>
+          </div>
+          <ul className={`menuNav-${menuOpen ? "menuOpen" : ""}`}>
+            <li>
+              <NavLink to="/">
+                Início
+                <FontAwesomeIcon className="icon-header" icon={faArrowDown} />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">
+                Sobre Nós
+                <FontAwesomeIcon className="icon-header" icon={faArrowDown} />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">
+                Serviços
+                <FontAwesomeIcon className="icon-header" icon={faArrowDown} />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">
+                Notícias
+                <FontAwesomeIcon className="icon-header" icon={faArrowDown} />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/">
+                Contactos
+                <FontAwesomeIcon className="icon-header" icon={faArrowDown} />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
